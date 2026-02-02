0db004cc77e0c8c7fe471d125dc85a5a8cfb6a7b
FieryRMS <raiyanmohdshahrms@gmail.com>
2026-02-02 16:28:50 +0800

    delete welcome page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,12 @@
 import { HomeIcon } from "lucide-react";
 import type { Route } from "../+types/root";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
     return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
 }
 
 export default function Home() {
-    return <Welcome />;
+    return <>index</>;
 }
 
 export const handle: PageHandle = {
