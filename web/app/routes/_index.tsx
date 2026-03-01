be5b35aaaaf3d0f4c9eb6e4a513e3491ea944308
FieryRMS <raiyanmohdshahrms@gmail.com>
2026-03-01 21:26:55 +0800

    move code

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,43 +1,4 @@
-import type { Route } from "./+types/_index";
-import { Category } from "@/components/category";
-import { fetchProducts } from "@/lib/api";
-import type { PageHandle, Product } from "@/types";
-import { useEffect } from "react";
-import { useFetcher, type UIMatch } from "react-router";
+import MainPage, { clientAction, meta } from "./c.$categoryId";
 
-export function meta({ matches }: Route.MetaArgs) {
-    const breadcrumbs = (matches as UIMatch<unknown, PageHandle>[])
-        .filter((match) => match.handle && match.handle.breadcrumb)
-        .map((match) => match.handle!.breadcrumb!(match));
-    const name = breadcrumbs.length ? breadcrumbs.at(-1)!.name : null;
-    return [
-        { title: `${name ? `${name} | ` : ""}The Generic Company` },
-        { name: "description", content: "A generic company that sells generic products" },
-    ];
-}
-
-export async function clientAction(): Promise<Product[]> {
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate network delay
-    return await fetchProducts(0, 30);
-}
-
-export default function MainPage() {
-    const productsFetcher = useFetcher<Product[]>();
-    useEffect(() => {
-        if (productsFetcher.state === "idle" && !productsFetcher.data) {
-            productsFetcher.submit({}, { method: "post" });
-        }
-    }, [productsFetcher]);
-
-    const products: Product[] =
-        productsFetcher.data ||
-        Array(10).fill({
-            id: "",
-            name: "",
-            imageUrls: [""],
-            desc: "",
-            price: 0,
-        });
-
-    return <Category products={products} />;
-}
+export { clientAction, meta };
+export default MainPage;
--- web/app/routes/c.$categoryId.tsx
+++ web/app/routes/c.$categoryId.tsx
@@ -1,11 +1,54 @@
 import { PascalCase } from "@/lib/utils";
-import type { PageHandle } from "@/types";
+import type { PageHandle, Product } from "@/types";
+import { useEffect } from "react";
 
-import { clientAction, meta } from "./_index";
-import MainPage from "./_index";
+import type { Route } from "./+types/c.$categoryId";
+import { fetchProducts } from "@/lib/api";
+import { useFetcher, type UIMatch } from "react-router";
+import { Category } from "@/components/category";
 
-export { clientAction, meta };
-export default MainPage;
+
+export function meta({ matches }: Route.MetaArgs) {
+    const breadcrumbs = (matches as UIMatch<unknown, PageHandle>[])
+        .filter((match) => match.handle && match.handle.breadcrumb)
+        .map((match) => match.handle!.breadcrumb!(match));
+    const name = breadcrumbs.length ? breadcrumbs.at(-1)!.name : null;
+    return [
+        { title: `${name ? `${name} | ` : ""}The Generic Company` },
+        { name: "description", content: "A generic company that sells generic products" },
+    ];
+}
+
+
+export async function clientAction(): Promise<Product[]> {
+    await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate network delay
+    return await fetchProducts(0, 30);
+}
+
+export async function loader({ params }: Route.LoaderArgs) {
+};
+
+export default function MainPage() {
+    const productsFetcher = useFetcher<Product[]>();
+    useEffect(() => {
+        if (productsFetcher.state === "idle" && !productsFetcher.data) {
+            productsFetcher.submit({}, { method: "post" });
+        }
+    }, [productsFetcher]);
+
+    const products: Product[] =
+        productsFetcher.data ||
+        Array(10).fill({
+            id: "",
+            name: "",
+            imageUrls: [""],
+            desc: "",
+            price: 0,
+        });
+
+    return <Category products={products} />;
+}
+
 
 export const handle: PageHandle = {
     breadcrumb: ({ params, id, pathname }) => ({
